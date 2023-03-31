5f30a183cf3827be109b776bfd75de99bb3b6ae2
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-04-01 00:41:53 +0300

    Adds recipes popular path docs

--- src/docs/paths/recipes/index.js
+++ src/docs/paths/recipes/index.js
@@ -1,7 +1,8 @@
 module.exports = {
   '/recipes': require('./recipes'),
-  '/recipes/{recipeId}': require('./recipe-id'),
   '/recipes/main-page': require('./main-page'),
+  '/recipes/popular': require('./popular'),
+  '/recipes/{recipeId}': require('./recipe-id'),
   '/recipes/category/{categoryName}': require('./category'),
   '/recipes/category-list': require('./category-list'),
 }
