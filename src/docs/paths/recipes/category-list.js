4379399fc0d4fb35202b4cec61474cdbfcfea846
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-04-01 00:19:06 +0300

    Updates docs category list order

--- src/docs/paths/recipes/category-list.js
+++ src/docs/paths/recipes/category-list.js
@@ -1,8 +1,8 @@
 module.exports = {
   get: {
     tags: ['Recipes'],
-    summary: 'Get the recipes categories list',
-    description: 'This route return the recipes categories list',
+    summary: 'Get categories list',
+    description: 'This route returns categories list',
     operationId: 'categoryList',
     security: [
       {
--- src/docs/paths/recipes/index.js
+++ src/docs/paths/recipes/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   '/recipes': require('./recipes'),
   '/recipes/{recipeId}': require('./recipe-id'),
-  '/recipes/category-list': require('./category-list'),
   '/recipes/main-page': require('./main-page'),
   '/recipes/category/{categoryName}': require('./category'),
+  '/recipes/category-list': require('./category-list'),
 }
