118f6aabcb468e2f95a7d581fd558feeb1ef508a
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-04-02 00:01:56 +0300

    Adds paginated response middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,9 @@
 const errorHandler = require('./errorHandler')
 const { bodyValidation } = require('./bodyValidation')
+const paginatedResponse = require('./paginatedResponse')
 
 module.exports = {
   errorHandler,
   bodyValidation,
+  paginatedResponse,
 }
