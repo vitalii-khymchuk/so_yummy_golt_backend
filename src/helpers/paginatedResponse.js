d31d57be567dfdb76b02eaa1e4d2f938b0420c6c
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-04-02 15:37:49 +0300

    Adds paginated response helper

---
+++ src/helpers/paginatedResponse.js
@@ -0,0 +1,37 @@
+const { BASE_URL } = process.env
+
+/**
+ * getPageUrl
+ * @param {string} path
+ * @param {number} page
+ * @returns {string}
+ */
+const getPageUrl = (path, page) => {
+  return `${BASE_URL}/api/v1${path}?page=${page}`
+}
+
+/**
+ * paginatedResponse
+ * @param {number} total
+ * @param {array} data
+ * @param {number} limit
+ * @param {number} page
+ * @param {string} path
+ * @returns {object}
+ */
+const paginatedResponse = (total, data, limit, page, path) => {
+  const totalPages = Math.ceil(total / limit)
+  const nextPageUrl = page >= totalPages ? null : getPageUrl(path, page + 1)
+
+  return {
+    total,
+    current_page: page,
+    per_page: limit,
+    data,
+    first_page_url: getPageUrl(path, 1),
+    last_page_url: getPageUrl(path, totalPages),
+    next_page_url: nextPageUrl,
+  }
+}
+
+module.exports = { paginatedResponse }
