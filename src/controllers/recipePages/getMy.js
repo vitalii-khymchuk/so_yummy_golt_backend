4cd54c6caba5f68d2d2533ade2bbfa04f11c5e00
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-04-04 12:05:33 +0300

    add pagination to myRecipes page

--- src/controllers/recipePages/getMy.js
+++ src/controllers/recipePages/getMy.js
@@ -1,10 +1,20 @@
 const asyncHandler = require('express-async-handler')
 const { RecipesService } = require('@services')
+const { paginatedResponse } = require('@helpers')
 
 const getMy = async (req, res) => {
   const { id } = req.user
-  const recipes = await RecipesService.getUserRecipes(id)
-  res.status(200).json({ code: 200, data: recipes })
+  const { path } = req.route
+  const { page, skip, limit } = req.paginatedResponse
+
+  const { total, data } = await RecipesService.getUserRecipes(id, {
+    skip,
+    limit,
+  })
+
+  const result = paginatedResponse(total, data, limit, page, path)
+
+  res.status(200).json({ code: 200, data: result })
 }
 
 module.exports = { getMy: asyncHandler(getMy) }
