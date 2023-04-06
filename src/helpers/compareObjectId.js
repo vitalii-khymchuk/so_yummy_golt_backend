a06d12665099f5370def9b142522ea2afd22b578
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-04-06 11:34:08 +0300

    fixed compareObjectId  issue

--- src/helpers/compareObjectId.js
+++ src/helpers/compareObjectId.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose')
 
 const compareObjectId = (id1 = '', id2 = '') => {
-  if (!(id1 instanceof mongoose.Types.ObjectId)) {
-    id1 = mongoose.Types.ObjectId(id1)
-  }
-  if (!(id2 instanceof mongoose.Types.ObjectId)) {
-    id2 = mongoose.Types.ObjectId(id2)
-  }
+  try {
+    if (!(id1 instanceof mongoose.Types.ObjectId)) {
+      id1 = mongoose.Types.ObjectId(id1)
+    }
+    if (!(id2 instanceof mongoose.Types.ObjectId)) {
+      id2 = mongoose.Types.ObjectId(id2)
+    }
 
-  return id1.equals(id2)
+    return id1.equals(id2)
+  } catch (error) {
+    return false
+  }
 }
 
 module.exports = { compareObjectId }
