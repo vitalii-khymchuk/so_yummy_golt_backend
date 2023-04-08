1953210e3e96b61ee499f2ba1042a6c02a64d41f
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-04-09 02:44:17 +0300

    Updates shopping list item delete docs

--- src/docs/paths/shopping-list/item-delete.js
+++ src/docs/paths/shopping-list/item-delete.js
@@ -59,7 +59,84 @@ module.exports = {
                 },
                 {
                   properties: {
-                    data: null,
+                    data: {
+                      type: 'array',
+                      description: 'Shopping list products',
+                      items: {
+                        type: 'object',
+                        required: ['id', 'recipeId', 'amount', 'measure'],
+                        properties: {
+                          id: {
+                            type: 'string',
+                            description: "Ingredient's item ID",
+                            example: '640c2dd963a319ea671e372c',
+                          },
+                          recipeId: {
+                            type: 'array',
+                            description: "Recipe's IDs",
+                            items: {
+                              type: 'string',
+                            },
+                            example: [
+                              '640c2dd963a319ea671e372c',
+                              '6d93a312d71e372c9ea6640c',
+                            ],
+                          },
+                          amount: {
+                            type: 'string',
+                            description: "Ingredient's item amount (quantity)",
+                            example: '1',
+                          },
+                          measure: {
+                            type: 'string',
+                            description: "Ingredient's item measurement units",
+                            example: 'tbsp',
+                          },
+                          ttl: {
+                            type: 'string',
+                            description: "Ingredient's item title",
+                            example: 'Sugar',
+                          },
+                          desc: {
+                            type: 'string',
+                            description: "Ingredient's item measurement units",
+                            example: 'tbsp',
+                          },
+                          thb: {
+                            type: 'string',
+                            description: "Ingredient's image URL",
+                            example:
+                              'https://s.gravatar.com/avatar/068de491621f7014bb5f8b3d473f50a3?s=250',
+                          },
+                        },
+                      },
+                      example: [
+                        {
+                          id: '640c2dd963a319ea671e373b',
+                          recipeId: [
+                            '642b391c5c598e54d1f9e71b',
+                            '642b391c5c598e54d1f9e71b',
+                          ],
+                          amount: 2,
+                          measure: 'pcs',
+                          ttl: 'Peanuts',
+                          desc: 'A legume with a thin, papery skin and a high oil content, commonly eaten roasted or boiled as a snack, or used in cooking and baking.',
+                          thb: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564856/ltc7yw8nvydf1bryb262.png',
+                        },
+                        {
+                          id: '640c2dd963a319ea671e373b',
+                          recipeId: [
+                            '640cd5ac2d9fecf12e889824',
+                            '640cd5ac2d9fecf12e88981f',
+                          ],
+                          amount: 7,
+                          measure: 'tbsp',
+                          ttl: 'Peanuts',
+                          desc: 'A legume with a thin, papery skin and a high oil content, commonly eaten roasted or boiled as a snack, or used in cooking and baking.',
+                          thb: 'https://res.cloudinary.com/ddbvbv5sp/image/upload/v1678564856/ltc7yw8nvydf1bryb262.png',
+                        },
+                      ],
+                    },
                   },
                 },
               ],
