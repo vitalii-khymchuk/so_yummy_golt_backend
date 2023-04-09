f931988b69c8e7d1ac829d0b1bbce3131f845339
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-04-09 17:28:04 +0300

    Updates body validation

--- src/middlewares/bodyValidation.js
+++ src/middlewares/bodyValidation.js
@@ -2,6 +2,9 @@ const { HttpError } = require('@helpers')
 
 const bodyValidation = schema => {
   return (req, res, next) => {
+    if (req.body?.jsonData) {
+      req.body = JSON.parse(req.body.jsonData)
+    }
     const { error } = schema.validate(req.body)
     if (error) {
       next(HttpError(400, error.message))
