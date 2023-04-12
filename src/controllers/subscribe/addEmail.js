729006c55b111696ad9a57629ce1db985b5ad8d6
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-04-13 00:49:37 +0300

    Updates subscibe add email controller

--- src/controllers/subscribe/addEmail.js
+++ src/controllers/subscribe/addEmail.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const { SubscriptionService, sendEmail } = require('@services')
-const { HttpError } = require('@helpers')
+const { HttpError, emails } = require('@helpers')
 
 const addEmail = async (req, res) => {
   const { email } = req.body
@@ -15,31 +15,7 @@ const addEmail = async (req, res) => {
     throw HttpError(500)
   }
 
-  const letter = `
-    <h1 style="color:#8BAA36;text-align:center;font-family:'Segoe UI',Tahoma,Verdana,Arial,sans-serif">
-        Be the first to see the new recipes!
-    </h1>
-    <br>
-    <p style="font-size:14px;font-family:'Segoe UI',Tahoma,Verdana,Arial,sans-serif">
-      We’ll now keep you posted about new recipes.
-    </p>
-    <br>
-    <p style="font-size:14px;font-family:'Segoe UI',Tahoma,Verdana,Arial,sans-serif">
-      Thanks,<br>
-      The SoYummy team
-    </p>
-    <p style="color:grey;font-size:80%;font-family:'Segoe UI',Tahoma,Verdana,Arial,sans-serif">
-      You’ve received this email because you signed up for a SoYummy newsletter
-    </p>
-  `
-
-  const text = `
-    Be the first to see the new recipes!\n\r\n\r
-    We’ll now keep you posted about new recipes.\n\r\n\r
-    Thanks,\n\r
-    The SoYummy team\n\r
-    You’ve received this email because you signed up for a SoYummy newsletter
-  `
+  const { letter, text } = emails.subscribe.addEmailTemplate()
 
   await sendEmail(
     email,
