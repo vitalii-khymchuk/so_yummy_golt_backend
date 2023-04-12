990a12774b87dd46436238575b0ffd40bb590ae5
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-04-12 10:37:48 +0300

    Adds recipe ingredient docs schema

--- src/docs/components/schemas/Recipe.js
+++ src/docs/components/schemas/Recipe.js
@@ -46,25 +46,7 @@ module.exports = {
         type: 'array',
         description: "Recipe's ingredients list",
         items: {
-          type: 'object',
-          required: ['id', 'amount', 'measure'],
-          properties: {
-            id: {
-              type: 'string',
-              description: "Ingredient's item ID",
-              example: '640c2dd963a319ea671e372c',
-            },
-            amount: {
-              type: 'string',
-              description: "Ingredient's item amount (quantity)",
-              example: '1',
-            },
-            measure: {
-              type: 'string',
-              description: "Ingredient's item measurement units",
-              example: 'tbsp',
-            },
-          },
+          $ref: '#/components/schemas/RecipeIngredient',
         },
       },
       thumb: {
