ca03329b94779971947253a54f68043767cf7361
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-04-12 15:44:44 +0300

    upgrade google user auth

--- src/controllers/auth/signInGoogle.js
+++ src/controllers/auth/signInGoogle.js
@@ -1,28 +1,21 @@
 const { HttpError } = require('@helpers')
 const asyncHandler = require('express-async-handler')
-const { GoogleAuth } = require('@services')
-const { User } = require('@models')
+const { GoogleAuth, UserService } = require('@services')
 
 const signInGoogle = async (req, res, next) => {
   try {
     const { credential: token } = req.body
     const { name, email, picture } = await GoogleAuth.verify(token)
-    const user = { name, email, avatarUrl: picture }
+    const userData = { name, email, avatarUrl: picture }
 
-    const options = {
-      upsert: true, // create user if not exists
-      new: true, // return the newly created/updated user object
-      setDefaultsOnInsert: true, // set default values when creating new document
-      defaults: { createdAt: Date.now() }, // set createdAt to current date when creating new document
-    }
-
-    const { favorites, shoppingList, recipes } = await User.findOneAndUpdate(
-      { email },
-      { ...user, token, password: '' },
-      options
+    const { favorites, shoppingList, recipes } = await UserService.signInGoogle(
+      {
+        ...userData,
+        token,
+        password: '',
+      }
     )
-
-    const data = { ...user, favorites, shoppingList, recipes }
+    const data = { ...userData, favorites, shoppingList, recipes }
 
     res.status(200).json({ code: 200, message: 'success', data, token })
   } catch (error) {
